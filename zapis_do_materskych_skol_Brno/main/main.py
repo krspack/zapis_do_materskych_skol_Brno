2bd8189e74e09e52ace0409feff5a75e793e248f
tyna <vojtiskova.kristyna@gmail.com>
2024-02-16 13:52:11 +0100

    hotovo, vcetne black

--- zapis_do_materskych_skol_Brno/main/main.py
+++ zapis_do_materskych_skol_Brno/main/main.py
@@ -14,7 +14,6 @@
 #
 
 
-
 import csv
 import pandas as pd
 import copy
@@ -32,28 +31,41 @@ script_name = sys.argv[0]
 oznaceni_testu = str(sys.argv[1] if len(sys.argv) > 1 else 1)
 
 # mezivýsledky uložené skriptem test.py. Pokud se main.py spouští samostatně (ne z testu), je nutné upravit cestu k souborům na "testy/{}_test/deti_test.csv"
-deti = pd.read_csv("{}_test/deti_test.csv".format(oznaceni_testu), dtype = {
-    'dite_id': 'string',
-    'spadova_skolka': 'string',
-    'skolka_sourozence': 'string'})
+deti = pd.read_csv(
+    "{}_test/deti_test.csv".format(oznaceni_testu),
+    dtype={
+        "dite_id": "string",
+        "spadova_skolka": "string",
+        "skolka_sourozence": "string",
+    },
+)
 
-skolky = pd.read_csv("{}_test/skolky_test.csv".format(oznaceni_testu), dtype = {
-    'skolka_id': 'string'})
+skolky = pd.read_csv(
+    "{}_test/skolky_test.csv".format(oznaceni_testu), dtype={"skolka_id": "string"}
+)
 
-prihlasky = pd.read_csv("{}_test/prihlasky_test.csv".format(oznaceni_testu), dtype = {
-   'dite': 'string',
-   'skolka': 'string'})
+prihlasky = pd.read_csv(
+    "{}_test/prihlasky_test.csv".format(oznaceni_testu),
+    dtype={"dite": "string", "skolka": "string"},
+)
 
-body = pd.read_csv("{}_test/body_test.csv".format(oznaceni_testu), dtype = {
-    'dite_id': 'string',
-    'spadova_skolka': 'string',
-    'skolka_sourozence': 'string'})
+body = pd.read_csv(
+    "{}_test/body_test.csv".format(oznaceni_testu),
+    dtype={
+        "dite_id": "string",
+        "spadova_skolka": "string",
+        "skolka_sourozence": "string",
+    },
+)
+
 
 # vstup pro funkci "match" c. 1: priority uchazecu
 def get_priorities(tabulka_prihlasek):
     # Funkce vytvoří slovník uchazečů a jejich prioritních seznamů školek.
-    prihlasky_groupedby_kids = tabulka_prihlasek.groupby('dite')['skolka'].agg(list)
+    prihlasky_groupedby_kids = tabulka_prihlasek.groupby("dite")["skolka"].agg(list)
     return prihlasky_groupedby_kids.to_dict()
+
+
 priority = get_priorities(prihlasky)
 
 """
@@ -73,32 +85,41 @@ for a, b in priority_jmena.items():
     print(a, b)
 """
 
+
 # vstup pro funkci "match" c. 2: kapacity skolek
 def get_volna_mista(df_skolky):
     # vyrobí slovník "školka: kapacita"
-    volna_mista = df_skolky[['skolka_id', 'volna_mista']]
-    volna_mista = skolky.set_index('skolka_id')['volna_mista'].to_dict()
-    volna_mista = {str(k):v for k, v in volna_mista.items()}
+    volna_mista = df_skolky[["skolka_id", "volna_mista"]]
+    volna_mista = skolky.set_index("skolka_id")["volna_mista"].to_dict()
+    volna_mista = {str(k): v for k, v in volna_mista.items()}
     return volna_mista
+
+
 volna_mista = get_volna_mista(skolky)
+
 
 # vstup pro funkci "match" c. 3: priority skolek, vyjadrene poradim obodovanych uchazecu o kazdou skolku
 def get_schools_longlists(body_df, prihlasky_df):
     # Z tabulky body_df vybere jen relevantni udaje podle toho, kdo se kam hlásí.
     # výstup je slovnik typu "skolka a k ni vsechny deti, co se na ni hlasi, seřazené podle počtu bodů"
-    prihlasky_groupedby_schools = prihlasky_df.groupby('skolka')['dite'].agg(list)
+    prihlasky_groupedby_schools = prihlasky_df.groupby("skolka")["dite"].agg(list)
     prihlasky_groupedby_schools = prihlasky_groupedby_schools.to_dict()
     longlists = {}
     schools = prihlasky_groupedby_schools.keys()
     for s in schools:
-        all_kids_one_school = body_df[['dite_id', str(s)]].copy()
-        all_kids_one_school['dite_id'] = all_kids_one_school['dite_id'].astype(str)
-        kids_applyint_to_one_school = all_kids_one_school.loc[all_kids_one_school['dite_id'].isin(prihlasky_groupedby_schools[str(s)])].copy()
-        kids_applyint_to_one_school.sort_values(by=str(s), ascending=False, inplace=True)
+        all_kids_one_school = body_df[["dite_id", str(s)]].copy()
+        all_kids_one_school["dite_id"] = all_kids_one_school["dite_id"].astype(str)
+        kids_applyint_to_one_school = all_kids_one_school.loc[
+            all_kids_one_school["dite_id"].isin(prihlasky_groupedby_schools[str(s)])
+        ].copy()
+        kids_applyint_to_one_school.sort_values(
+            by=str(s), ascending=False, inplace=True
+        )
         kids_applyint_to_one_school.reset_index(inplace=True, drop=True)
-        sorted_longlist = tuple(kids_applyint_to_one_school['dite_id'])
+        sorted_longlist = tuple(kids_applyint_to_one_school["dite_id"])
         longlists[str(s)] = [str(x) for x in sorted_longlist]
     return longlists
+
 
 serazeni_uchazeci = get_schools_longlists(body, prihlasky)
 
@@ -125,37 +146,58 @@ print('školky a jejich kapacity: ', volna_mista)
 
 # ------------------------------------------------------------------------------------
 sys.setrecursionlimit(10000)
+
+
 def match(priority_zaku, priority_skolek, kapacity_skolek):
-    game = HospitalResident.create_from_dictionaries(priority_zaku, priority_skolek, kapacity_skolek)
+    game = HospitalResident.create_from_dictionaries(
+        priority_zaku, priority_skolek, kapacity_skolek
+    )
     schools_shortlists = game.solve(optimal="resident")
     return schools_shortlists
+
+
 rozrazeni = match(priority, serazeni_uchazeci, volna_mista)
 
+
 def save_results(results, test_number):
-    nazev_adresare = test_number + '_test'
-    with open('{}/vysledek.csv'.format(nazev_adresare), 'w') as file:
-        writer = csv.DictWriter(file, fieldnames = ['dite', 'skolka', 'poradi'])
+    nazev_adresare = test_number + "_test"
+    with open("{}/vysledek.csv".format(nazev_adresare), "w") as file:
+        writer = csv.DictWriter(file, fieldnames=["dite", "skolka", "poradi"])
         writer.writeheader()
         for school, shortlist in results.items():
             for index, person in enumerate(shortlist):
-                d = {'skolka': school, 'poradi': index + 1, 'dite': person}
+                d = {"skolka": school, "poradi": index + 1, "dite": person}
                 writer.writerow(d)
     return
+
+
 save_results(rozrazeni, oznaceni_testu)
+
 
 def get_vysledek_se_jmeny(vysledek, deti_df):
     names = {}
     for k, v in vysledek.items():
-        k_nazev = skolky.loc[skolky['skolka_id'] == k.name,'nazev_kratky'].item()
-        v_jmeno = [(deti_df.loc[deti_df['dite_id'] == i.name, 'jmeno'].item() + ' (' + i.name + ')') for i in v]
-        names[k_nazev + ' (' + k.name + ')'] = v_jmeno
+        k_nazev = skolky.loc[skolky["skolka_id"] == k.name, "nazev_kratky"].item()
+        v_jmeno = [
+            (
+                deti_df.loc[deti_df["dite_id"] == i.name, "jmeno"].item()
+                + " ("
+                + i.name
+                + ")"
+            )
+            for i in v
+        ]
+        names[k_nazev + " (" + k.name + ")"] = v_jmeno
     return names
+
+
 vysledek_se_jmeny = get_vysledek_se_jmeny(rozrazeni, deti)
-print('Výsledky - kdo se dostal kam: ')
+print("Výsledky - kdo se dostal kam: ")
 print()
-for k,v in vysledek_se_jmeny.items():
-    print(k,v)
+for k, v in vysledek_se_jmeny.items():
+    print(k, v)
     print()
+
 
 def kdo_se_nedostal(vysledek, deti_df):
     uspesni_uchazeci = []
@@ -164,20 +206,15 @@ def kdo_se_nedostal(vysledek, deti_df):
     uspesni_uchazeci = [_.name for _ in uspesni_uchazeci]
 
     neuspesni = {}
-    for _ in deti_df['dite_id']:
+    for _ in deti_df["dite_id"]:
         if _ not in uspesni_uchazeci:
-            neuspesni[_] = deti_df.loc[deti_df['dite_id'] == _, 'jmeno'].item()
+            neuspesni[_] = deti_df.loc[deti_df["dite_id"] == _, "jmeno"].item()
     return neuspesni
+
+
 print()
 print()
-print('Neúspěšní uchazeči:')
+print("Neúspěšní uchazeči:")
 print()
 for k, v in kdo_se_nedostal(rozrazeni, deti).items():
-    print (v + ' (' + str(k) + ')')
-
-
-
-
-
-
-
+    print(v + " (" + str(k) + ")")
--- zapis_do_materskych_skol_Brno/main/testy/test.py
+++ zapis_do_materskych_skol_Brno/main/testy/test.py
@@ -18,14 +18,17 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # vstupni data
 ## fiktivni uchazeci - viz vyroba_testovacich_dat.py
-deti = pd.read_csv('deti.csv', dtype = {
-    'dite_id': 'string',
-    'spadova_skolka': 'string',
-    'skolka_sourozence': 'int64'})
+deti = pd.read_csv(
+    "deti.csv",
+    dtype={
+        "dite_id": "string",
+        "spadova_skolka": "string",
+        "skolka_sourozence": "int64",
+    },
+)
 
 ## skutecne skolky
-skolky = pd.read_csv('skolky.csv', dtype = {
-    'skolka_id': 'string'})
+skolky = pd.read_csv("skolky.csv", dtype={"skolka_id": "string"})
 
 
 # parametry testu ctene z prikazove radky
@@ -33,64 +36,85 @@ script_name = sys.argv[0]
 pocet_deti = int(sys.argv[1] if len(sys.argv) > 1 else 4488)
 pocet_skolek = int(sys.argv[2] if len(sys.argv) > 2 else len(skolky))
 random_seed = int(sys.argv[3] if len(sys.argv) > 3 else 42)
-kapacita_jedne_skolky = int(sys.argv[4] if len(sys.argv) > 4 else -1)  # defaultne: skutecne kapacity.
-cislo_testu = str(sys.argv[5] if len(sys.argv) > 5 else 1)
+cislo_testu = str(sys.argv[4] if len(sys.argv) > 4 else 1)
+kapacita_jedne_skolky = int(
+    sys.argv[5] if len(sys.argv) > 5 else -1
+)  # defaultne: skutecne kapacity.
+
 
 random.seed(random_seed)
 
+
 def vyrob_adresar(test_number):
-    if test_number not in [1, '1']:
-        nazev_adresare = str(test_number) + '_test'
-        vyrob_novy_adresar =  ["mkdir", nazev_adresare]
+    if test_number not in [1, "1"]:
+        nazev_adresare = str(test_number) + "_test"
+        vyrob_novy_adresar = ["mkdir", nazev_adresare]
         subprocess.run(vyrob_novy_adresar)
     return
+
+
 vyrob_adresar(cislo_testu)
+
 
 def get_kapacity_skolek(kapacity_input, skolky_df):
     if kapacity_input > 0:
-        skolky_df['volna_mista'] = kapacity_input
+        skolky_df["volna_mista"] = kapacity_input
     return
+
+
 get_kapacity_skolek(kapacita_jedne_skolky, skolky)
+
 
 def get_random_rows_skolky(dataframe, num_rows):
     # vybere nahodne skolky z datasetu 'skolky' v poctu pocet_skolek
     random_rows = dataframe.sample(n=num_rows)
     random_rows.reset_index()
     return random_rows
-skolky = get_random_rows_skolky(skolky, num_rows = pocet_skolek)
 
-skolky.to_csv('{}_test/skolky_test.csv'.format(cislo_testu))
+
+skolky = get_random_rows_skolky(skolky, num_rows=pocet_skolek)
+
+skolky.to_csv("{}_test/skolky_test.csv".format(cislo_testu))
+
 
 def get_random_rows_deti(dataframe, num_rows):
     # Vybere nahodne uchazece z datasetu 'deti' v poctu pocet_uchazecu
     random_rows = dataframe.sample(n=num_rows)
-    random_rows['spadova_skolka'] = [random.choice(list(skolky.skolka_id)) for _ in range(num_rows)]
-    random_rows['skolka_sourozence'] = [int(random.choice(list(skolky.skolka_id))) if original_value >= 0 else original_value
-        for original_value in random_rows['skolka_sourozence']]
+    random_rows["spadova_skolka"] = [
+        random.choice(list(skolky.skolka_id)) for _ in range(num_rows)
+    ]
+    random_rows["skolka_sourozence"] = [
+        int(random.choice(list(skolky.skolka_id)))
+        if original_value >= 0
+        else original_value
+        for original_value in random_rows["skolka_sourozence"]
+    ]
     random_rows.reset_index()
     return random_rows
 
-deti = get_random_rows_deti(deti, num_rows = pocet_deti)
 
-deti.to_csv('{}_test/deti_test.csv'.format(cislo_testu))
+deti = get_random_rows_deti(deti, num_rows=pocet_deti)
+
+deti.to_csv("{}_test/deti_test.csv".format(cislo_testu))
+
 
 # vyroba prihlasek:
 def get_prihlasky(deti_df, skolky_df):
     # Fuknce simuluje výběr oblíbených školek. Počet položek je náhodný.
     # Funkce při "výběru" oblíbených školek zohledňuje spádovost a školku sourozence.
-    data = {'dite': [], 'jmeno': [], 'skolka': [], 'poradi': []}
+    data = {"dite": [], "jmeno": [], "skolka": [], "poradi": []}
     for idx, row in deti_df.iterrows():
         vybrane_skolky = set()
         pocet_prihlasek = random.randint(1, len(skolky_df))
-        dostupne_skolky= list(skolky_df.skolka_id)
+        dostupne_skolky = list(skolky_df.skolka_id)
         dostupne_skolky = [int(_) for _ in dostupne_skolky]
         for i in range(pocet_prihlasek):
             if i == 0:
-                choice = int(row['spadova_skolka'])
+                choice = int(row["spadova_skolka"])
                 vybrane_skolky.add(choice)
                 dostupne_skolky = [_ for _ in dostupne_skolky if _ != choice]
-            elif (i == 1 and row['skolka_sourozence'] >= 0):
-                choice = int(row['skolka_sourozence'])
+            elif i == 1 and row["skolka_sourozence"] >= 0:
+                choice = int(row["skolka_sourozence"])
                 vybrane_skolky.add(choice)
                 dostupne_skolky = [_ for _ in dostupne_skolky if _ != choice]
             else:
@@ -103,129 +127,216 @@ def get_prihlasky(deti_df, skolky_df):
         vybrane_skolky = [str(_) for _ in vybrane_skolky]
 
         for index, value in enumerate(vybrane_skolky):
-            data['dite'].append(str(row['dite_id']))
-            data['jmeno'].append(row['jmeno'])
-            data['skolka'].append(value)
-            data['poradi'].append(index + 1)
+            data["dite"].append(str(row["dite_id"]))
+            data["jmeno"].append(row["jmeno"])
+            data["skolka"].append(value)
+            data["poradi"].append(index + 1)
 
     df = pd.DataFrame(data)
     return df
 
-prihlasky = get_prihlasky(deti, skolky)
-prihlasky.to_csv('{}_test/prihlasky_test.csv'.format(cislo_testu))
 
-print('Vstupní data pro rozřazení (vybrané sloupce, prvních 10 řádek): ')
+prihlasky = get_prihlasky(deti, skolky)
+prihlasky.to_csv("{}_test/prihlasky_test.csv".format(cislo_testu))
+
+print("Vstupní data pro rozřazení (vybrané sloupce, prvních 10 řádek): ")
 print()
-print('Uchazeči:')
-print(deti[['dite_id', 'jmeno', 'datum_narozeni', 'spadova_skolka']].head(10))
+print("Uchazeči:")
+print(deti[["dite_id", "jmeno", "datum_narozeni", "spadova_skolka"]].head(10))
 print()
-print('Školky:')
-print(skolky[['skolka_id', 'nazev_kratky', 'volna_mista']].head(10))
+print("Školky:")
+print(skolky[["skolka_id", "nazev_kratky", "volna_mista"]].head(10))
 print()
-print('Priority uchazečů:')
+print("Priority uchazečů:")
 print(prihlasky)
 
 
 # vypocet bodu pro jednotlive uchazece podle kritérií popsaných zde: https://zapisdoms.brno.cz/kriteria-rizeni
 
+
 def get_mestska_cast(deti_df):
     # Funkce rozšíří tabulku dětí o sloupec "mestska_cast", převzatý od spádové školky, za účelem obodování.
-    deti_df = pd.merge(deti_df, skolky[['skolka_id', 'mc']], left_on = 'spadova_skolka', right_on = 'skolka_id')
-    deti_df.drop('skolka_id', axis = 1, inplace = True)
+    deti_df = pd.merge(
+        deti_df,
+        skolky[["skolka_id", "mc"]],
+        left_on="spadova_skolka",
+        right_on="skolka_id",
+    )
+    deti_df.drop("skolka_id", axis=1, inplace=True)
     return deti_df
+
+
 deti = get_mestska_cast(deti)
+
 
 def get_age(birthday):
     # Z data narození vypočte věk v letech a dnech k letošnímu 31. srpnu.
     today = datetime.date.today()
     current_year = today.year
     schoolyear_start = datetime.date(birthday.year, 8, 31)
-    difference = schoolyear_start - birthday - relativedelta(days = 1)   # prizpusobeni vypoctu webu zapisdoms.brno.cz
+    difference = (
+        schoolyear_start - birthday - relativedelta(days=1)
+    )  # prizpusobeni vypoctu webu zapisdoms.brno.cz
 
     schoolyear_start_current = datetime.date(current_year, 8, 31)
-    age_in_years = int(round((schoolyear_start_current - schoolyear_start).days/365, 0))
+    age_in_years = int(
+        round((schoolyear_start_current - schoolyear_start).days / 365, 0)
+    )
     if difference.days < 0:
         age_in_years -= 1
     return age_in_years, difference.days
 
+
 def get_points_years(age, age_difference_days):
     # Přidelí body za věk.
     options = {7: 2160, 6: 2120, 5: 2080, 4: 2040, 3: 2000, 2: 0, 1: 0}
-    calculate_points = lambda x: 1000 if (x == 2 and age_difference_days < 0) else options[x]
+    calculate_points = (
+        lambda x: 1000 if (x == 2 and age_difference_days < 0) else options[x]
+    )
     points_years = calculate_points(age)
     return points_years
+
 
 def calculate_points_one_child(id_child):
     # Sečte body za všechna bodovaná kritéria.
 
     # vytvořit tabulku "1 uchazeč, všechny školky"
     copy_skolky = copy.deepcopy(skolky)
-    copy_skolky['dite_id'] = id_child
-    df_one_child = pd.merge(copy_skolky, deti, left_on ='dite_id', right_on = 'dite_id', how = 'left')
-    df_one_child.rename(columns={'mc_x': 'mc_skolka', 'mc_y': 'mc_dite'}, inplace = True)
+    copy_skolky["dite_id"] = id_child
+    df_one_child = pd.merge(
+        copy_skolky, deti, left_on="dite_id", right_on="dite_id", how="left"
+    )
+    df_one_child.rename(columns={"mc_x": "mc_skolka", "mc_y": "mc_dite"}, inplace=True)
 
     # body za věk
-    df_one_child['datum_narozeni'] = pd.to_datetime(df_one_child['datum_narozeni']).dt.date
-    df_one_child['vek'] = df_one_child['datum_narozeni'].apply(lambda x: pd.Series(get_age(x)))[0]
-    df_one_child['vek_dny_srpen31'] = df_one_child['datum_narozeni'].apply(lambda x: pd.Series(get_age(x)))[1]
-    df_one_child['body_za_vek_roky'] = df_one_child.apply(lambda row: get_points_years(row['vek'], row['vek_dny_srpen31']), axis=1)
-    df_one_child['body_za_vek_dny'] = df_one_child.apply(lambda row: 0 if (row['vek_dny_srpen31'] < 0) else row['vek_dny_srpen31']*0.02, axis = 1)
-    df_one_child['prioritni_vek'] = df_one_child.apply(lambda row: True if (3 <= row['vek'] <= 6) else False, axis = 1)
+    df_one_child["datum_narozeni"] = pd.to_datetime(
+        df_one_child["datum_narozeni"]
+    ).dt.date
+    df_one_child["vek"] = df_one_child["datum_narozeni"].apply(
+        lambda x: pd.Series(get_age(x))
+    )[0]
+    df_one_child["vek_dny_srpen31"] = df_one_child["datum_narozeni"].apply(
+        lambda x: pd.Series(get_age(x))
+    )[1]
+    df_one_child["body_za_vek_roky"] = df_one_child.apply(
+        lambda row: get_points_years(row["vek"], row["vek_dny_srpen31"]), axis=1
+    )
+    df_one_child["body_za_vek_dny"] = df_one_child.apply(
+        lambda row: 0
+        if (row["vek_dny_srpen31"] < 0)
+        else row["vek_dny_srpen31"] * 0.02,
+        axis=1,
+    )
+    df_one_child["prioritni_vek"] = df_one_child.apply(
+        lambda row: True if (3 <= row["vek"] <= 6) else False, axis=1
+    )
 
     # body za bydliště
-    df_one_child['body_spadovost'] = df_one_child.apply(lambda row: 250 if row['bydliste_brno'] == True else 0, axis = 1)
-    df_one_child['body_spadovost'] = df_one_child.apply(lambda row: 500 if row['mc_skolka'] == row['mc_dite'] else row['body_spadovost'], axis = 1)
-    df_one_child['body_spadovost'] = df_one_child.apply(lambda row: 750 if (row['skolka_id'] == row['spadova_skolka']) and (row['prioritni_vek'] == False) else row['body_spadovost'], axis = 1)
-    df_one_child['body_spadovost'] = df_one_child.apply(lambda row: 1000 if (row['skolka_id'] == row['spadova_skolka']) and (row['prioritni_vek'] == True) else row['body_spadovost'], axis = 1)
+    df_one_child["body_spadovost"] = df_one_child.apply(
+        lambda row: 250 if row["bydliste_brno"] == True else 0, axis=1
+    )
+    df_one_child["body_spadovost"] = df_one_child.apply(
+        lambda row: 500
+        if row["mc_skolka"] == row["mc_dite"]
+        else row["body_spadovost"],
+        axis=1,
+    )
+    df_one_child["body_spadovost"] = df_one_child.apply(
+        lambda row: 750
+        if (row["skolka_id"] == row["spadova_skolka"])
+        and (row["prioritni_vek"] == False)
+        else row["body_spadovost"],
+        axis=1,
+    )
+    df_one_child["body_spadovost"] = df_one_child.apply(
+        lambda row: 1000
+        if (row["skolka_id"] == row["spadova_skolka"])
+        and (row["prioritni_vek"] == True)
+        else row["body_spadovost"],
+        axis=1,
+    )
 
     # body za sourozence ve školce
-    df_one_child['skolka_sourozence'] = pd.to_numeric(df_one_child['skolka_sourozence'], errors='coerce') # nutne kvuli df.query nize
-    df_one_child['skolka_id'] = pd.to_numeric(df_one_child['skolka_id'], errors='coerce')
-    df_one_child['body_sourozenec'] = df_one_child.query('prioritni_vek == True and skolka_sourozence == skolka_id').apply(lambda x: 10, axis = 1)
-    df_one_child['body_sourozenec'].fillna(0, inplace = True)
-    df_one_child['skolka_sourozence'] = df_one_child['skolka_sourozence'].astype(str)
-    df_one_child['skolka_id'] = df_one_child['skolka_id'].astype(str)
+    df_one_child["skolka_sourozence"] = pd.to_numeric(
+        df_one_child["skolka_sourozence"], errors="coerce"
+    )  # nutne kvuli df.query nize
+    df_one_child["skolka_id"] = pd.to_numeric(
+        df_one_child["skolka_id"], errors="coerce"
+    )
+    df_one_child["body_sourozenec"] = df_one_child.query(
+        "prioritni_vek == True and skolka_sourozence == skolka_id"
+    ).apply(lambda x: 10, axis=1)
+    df_one_child["body_sourozenec"].fillna(0, inplace=True)
+    df_one_child["skolka_sourozence"] = df_one_child["skolka_sourozence"].astype(str)
+    df_one_child["skolka_id"] = df_one_child["skolka_id"].astype(str)
 
     # dve specialni skolky, které mají jako spádovou oblast celé Brno a nabízejí prodloužený provoz
     # body navíc, pokud uchazeč doloží potřebu prodlouženého provozu
     # zapisdoms.brno.cz nespecifikuje počet bodů navíc >>> arbitrárně stanoveno 50 bodů
-    df_one_child.loc[df_one_child['mc_skolka'] == "brno",'body_spadovost'] = df_one_child.loc[df_one_child['mc_skolka'] == "brno", 'body_spadovost'].apply(lambda x: 1000)
-    df_one_child.loc[df_one_child['mc_skolka'] == "brno",'body_sourozenec'] = df_one_child.loc[df_one_child['mc_skolka'] == "brno", 'body_sourozenec'].apply(lambda x: 10)
-    df_one_child['body_prodlouz_provoz'] = 0
-    df_one_child.loc[df_one_child['mc_skolka'] == "brno",'body_prodlouz_provoz'] = df_one_child.loc[df_one_child['mc_skolka'] == "brno", 'body_prodlouz_provoz'].apply(lambda x: 50 if 'prodlouzena_dochazka' == True else 0)
+    df_one_child.loc[
+        df_one_child["mc_skolka"] == "brno", "body_spadovost"
+    ] = df_one_child.loc[df_one_child["mc_skolka"] == "brno", "body_spadovost"].apply(
+        lambda x: 1000
+    )
+    df_one_child.loc[
+        df_one_child["mc_skolka"] == "brno", "body_sourozenec"
+    ] = df_one_child.loc[df_one_child["mc_skolka"] == "brno", "body_sourozenec"].apply(
+        lambda x: 10
+    )
+    df_one_child["body_prodlouz_provoz"] = 0
+    df_one_child.loc[
+        df_one_child["mc_skolka"] == "brno", "body_prodlouz_provoz"
+    ] = df_one_child.loc[
+        df_one_child["mc_skolka"] == "brno", "body_prodlouz_provoz"
+    ].apply(
+        lambda x: 50 if "prodlouzena_dochazka" == True else 0
+    )
 
     # nezohledneni bodů za "Den věku dítěte v roce narození" v případě spádových školek
-    df_one_child.loc[df_one_child['spadova_skolka'] == df_one_child['skolka_id'], 'body_za_vek_dny'] = df_one_child.loc[df_one_child['spadova_skolka'] == df_one_child['skolka_id'], 'body_za_vek_dny'].apply(lambda x: 0)
+    df_one_child.loc[
+        df_one_child["spadova_skolka"] == df_one_child["skolka_id"], "body_za_vek_dny"
+    ] = df_one_child.loc[
+        df_one_child["spadova_skolka"] == df_one_child["skolka_id"], "body_za_vek_dny"
+    ].apply(
+        lambda x: 0
+    )
 
     # součet bodů
-    df_one_child['body_soucet'] = df_one_child['body_sourozenec'] + df_one_child['body_spadovost'] + df_one_child['body_za_vek_roky'] + df_one_child['body_za_vek_dny'] + df_one_child['body_prodlouz_provoz']
-    d_pivoted = df_one_child.pivot(index = 'dite_id', columns = 'skolka_id', values = 'body_soucet')
-    d_pivoted = pd.merge(deti, d_pivoted, how = 'right', left_on = 'dite_id', right_on = 'dite_id')
+    df_one_child["body_soucet"] = (
+        df_one_child["body_sourozenec"]
+        + df_one_child["body_spadovost"]
+        + df_one_child["body_za_vek_roky"]
+        + df_one_child["body_za_vek_dny"]
+        + df_one_child["body_prodlouz_provoz"]
+    )
+    d_pivoted = df_one_child.pivot(
+        index="dite_id", columns="skolka_id", values="body_soucet"
+    )
+    d_pivoted = pd.merge(
+        deti, d_pivoted, how="right", left_on="dite_id", right_on="dite_id"
+    )
     return d_pivoted
 
+
 # výstup pro prvního uchazeče:
-body = calculate_points_one_child(deti.iloc[0]['dite_id'])
+body = calculate_points_one_child(deti.iloc[0]["dite_id"])
 
 # výstup pro všechny uchazeče:
 for i in range(1, len(deti)):
-    one_child_pivoted_df = calculate_points_one_child(deti.iloc[i]['dite_id'])
+    one_child_pivoted_df = calculate_points_one_child(deti.iloc[i]["dite_id"])
     body = pd.concat([body, one_child_pivoted_df])
-body[['dite_id', 'spadova_skolka', 'skolka_sourozence']] = body[['dite_id', 'spadova_skolka', 'skolka_sourozence']].astype('str')
-body.to_csv('{}_test/body_test.csv'.format(cislo_testu))
+body[["dite_id", "spadova_skolka", "skolka_sourozence"]] = body[
+    ["dite_id", "spadova_skolka", "skolka_sourozence"]
+].astype("str")
+body.to_csv("{}_test/body_test.csv".format(cislo_testu))
 
-print('Všichni uchazeči vs. všechny školky: přidělené body')
+print("Všichni uchazeči vs. všechny školky: přidělené body")
 print()
 seznam_id_skolek = list(skolky.skolka_id)
-print(body[['dite_id', 'jmeno'] + seznam_id_skolek])
+print(body[["dite_id", "jmeno"] + seznam_id_skolek])
 
 # -------------------------------------------------
 
 # spusteni skriptu main.py:
-spustit_main = ['python', '../main.py', cislo_testu]
+spustit_main = ["python", "../main.py", cislo_testu]
 subprocess.run(spustit_main)
-
-
-
-
-
-
